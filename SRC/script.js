7c491a880b61828932bbd0ca1bd03e8b52ccf982
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-10-19 09:43:19 +0300

    change and add design and script

--- SRC/script.js
+++ SRC/script.js
@@ -15,3 +15,9 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+function ion(e) {
+    e.preventDefault();
+
+    document.getElementById("demo").innerHTML = "Hello Dear Visitor!</br> We are happy that you've chosen our website to learn programming languages. We're sure you'll become one of the best programmers in your country. Good luck to you!";
+}
